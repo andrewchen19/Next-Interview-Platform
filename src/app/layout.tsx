218253ae677382bbd321e4ffb2d6ba15143f7c93
andrewchen19 <andrew831119work@gmail.com>
2025-05-12 16:45:21 +0800

    feat: add toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
@@ -8,7 +9,7 @@ const monaSans = Mona_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "AceAhead",
+  title: "HireReady",
   description: "An AI-powered platform for preparing for mock interviews.",
 };
 
@@ -21,7 +22,10 @@ export default function RootLayout({
     // guarantee only dark mode is applied
     <html lang="en" className="dark">
       {/* .className -> applied that font everywhere on the website */}
-      <body className={`${monaSans.className} antialiased`}>{children}</body>
+      <body className={`${monaSans.className} antialiased pattern`}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
