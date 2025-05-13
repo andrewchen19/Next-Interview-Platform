0598376d16ccd2d463e2ae1390e0925a2411e9ad
andrewchen19 <andrew831119work@gmail.com>
2025-05-13 13:20:45 +0800

    feat: use protecting route function

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,10 +1,17 @@
+import { isAuthenticated } from "@/actions/auth.action";
+import { redirect } from "next/navigation";
+
 import NavBar from "@/components/NavBar";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (!isUserAuthenticated) redirect("/sign-in");
+
   return (
     <div className="root-layout">
       <NavBar />
