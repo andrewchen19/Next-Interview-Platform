b747cf0b2c49831c8bbb0a8f14c97c5a65939a2d
andrewchen19 <andrew831119work@gmail.com>
2025-05-13 16:13:59 +0800

    feat: create Agent component and add into interview page

---
+++ src/app/(root)/interview/page.tsx
@@ -0,0 +1,11 @@
+import Agent from "@/components/Agent";
+
+export default function InterviewPage() {
+  return (
+    <>
+      <h3>Interview Generation</h3>
+
+      <Agent userName="Abby" userId="user1" type="generate" />
+    </>
+  );
+}
