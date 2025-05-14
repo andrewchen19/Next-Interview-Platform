76fefe388f0d50c5f5eccdeee612b4cf466da9fc
andrewchen19 <andrew831119work@gmail.com>
2025-05-14 12:28:41 +0800

    feat: create get interviews function

---
+++ src/actions/interview.action.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { db } from "@/firebase/admin";
+
+export async function getInterviewsByUserId(
+  userId: string
+): Promise<Interview[] | null> {
+  try {
+    const interviews = await db
+      .collection("interviews")
+      .where("userId", "==", userId)
+      .orderBy("createdAt", "desc")
+      .get();
+
+    return interviews.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    })) as Interview[];
+  } catch {
+    return null;
+  }
+}
+
+export async function getOthersInterviews(
+  params: GetLatestInterviewsParams
+): Promise<Interview[] | null> {
+  const { userId, limit = 8 } = params;
+
+  try {
+    const interviews = await db
+      .collection("interviews")
+      .where("userId", "!=", userId)
+      .where("finalized", "==", true)
+      .orderBy("createdAt", "desc")
+      .limit(limit)
+      .get();
+
+    return interviews.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    })) as Interview[];
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
