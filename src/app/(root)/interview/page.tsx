3d2fcd735f4e4737fbfb570146a462c5e461de8b
andrewchen19 <andrew831119work@gmail.com>
2025-05-14 17:35:58 +0800

    refactor(interview): delete no user condition

--- src/app/(root)/interview/page.tsx
+++ src/app/(root)/interview/page.tsx
@@ -5,13 +5,15 @@ import { getCurrentUser } from "@/actions/auth.action";
 export default async function InterviewPage() {
   const user = await getCurrentUser();
 
-  if (!user) return null;
-
   return (
     <>
       <h3>Interview Generation</h3>
 
-      <Agent userName={user.name} userId={user.id} type="generate" />
+      <Agent
+        userName={user?.name as string}
+        userId={user?.id as string}
+        type="generate"
+      />
     </>
   );
 }
