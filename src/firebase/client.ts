316704fe9d27acc767f6f216dfa7a5f436e83da8
andrewchen19 <andrew831119work@gmail.com>
2025-05-19 22:53:26 +0800

    fix: replace with environment variables

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDhl0-sJFNxl57t6OMOg7Myx60gjTU_q5Y",
-  authDomain: "interview-platform-b5894.firebaseapp.com",
-  projectId: "interview-platform-b5894",
-  storageBucket: "interview-platform-b5894.firebasestorage.app",
-  messagingSenderId: "891988782406",
-  appId: "1:891988782406:web:b821ffebf7c1a0728a4947",
-  measurementId: "G-LKRENM7P7T",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // safety check that ensures firebase is initialized only once
